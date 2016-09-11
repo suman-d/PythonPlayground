9c3e5b90d663c15e613f672060fe711b50ba9ba2
Suman Debnath <sd887851@avagotech.net>
2016-09-11 14:38:11 +0530

    Longest Substring

--- LongestStringCheck/check_longest_string.py
+++ LongestStringCheck/check_longest_string.py
@@ -1,9 +1,11 @@
+# Paste your code into this box
+
 import string
 
 
 def sub_string(st):
-    first_letter_index = string.lowercase.index(st[0])
-    first_letter_end_list = string.lowercase[first_letter_index::]
+    first_letter_index = string.ascii_lowercase.index(st[0])
+    first_letter_end_list = string.ascii_lowercase[first_letter_index::]
     result = {st: 0}
     for i in st:
         if i == first_letter_end_list[0]:
@@ -24,16 +26,13 @@ def check_for_longest_substring(test_string):
     index = 0
     for i, j in enumerate(list(test_string)):
         result = sub_string("".join(list(test_string)[i:]))
-
-        if result == max and j <= test_string[index]:
-            max = result
-            index = i
+        print i, j
+        if result == max:
+            continue
         elif result > max:
             max = result
             index = i
 
     return "".join(list(test_string)[index:(index+max):])
-
-
-
-print check_for_longest_substring("xxxiiiiiiiiiiaaabb")
+#s = "fyihrycjaljqyxs"
+print("Longest substring in alphabetical order is: ", check_for_longest_substring(s))
